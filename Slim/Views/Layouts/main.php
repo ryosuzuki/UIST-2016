c48bea9dbd7cd77caffb3cb5dd0fbf124d9d29f0
Ryo Suzuki <ryosuzk@gmail.com>
2015-07-06 20:07:07 -0700

    Change title uist 2016

--- Slim/Views/Layouts/main.php
+++ Slim/Views/Layouts/main.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8" name="viewport" content="width=device-width, user-scalable=no" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>UIST 2015: 28th ACM UIST Symposium</title>
+        <title>UIST 2016: 28th ACM UIST Symposium</title>
         <link rel="icon" type="image/png" href='<?php echo BASE_PATH; ?>/lib/img/icon-acm.gif'>
         <link rel='stylesheet' type='text/css' href='<?php echo BASE_PATH; ?>/lib/css/style.css' />
         <link rel='stylesheet' type='text/css' href='<?php echo BASE_PATH; ?>/lib/css/mason.css' />
