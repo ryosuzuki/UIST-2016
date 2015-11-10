ef2d2134a5cec14972fdd5086a2d77fabf0aabd5
Ryo Suzuki <ryosuzk@gmail.com>
2015-11-10 16:15:39 -0500

    Add new comittee member

--- Slim/Views/home.php
+++ Slim/Views/home.php
@@ -105,7 +105,7 @@
     <div class="ui stackable grid detail">
       <div class="seven wide column centered">
         <h3 class="text text-spaced text-center">
-        <span>General Chair</span>
+        <span>General Co-Chairs</span>
         </h3>
         <a href="http://lab.rekimoto.org/members-2/rekimoto/" target="_blank">
           <div style="width:50%;float:left;">
@@ -123,7 +123,7 @@
 
       <div class="seven wide column centered">
         <h3 class="text text-spaced text-center">
-        <span>Program Co-Chair</span>
+        <span>Program Committee Co-Chairs</span>
         </h3>
         <a href="https://faculty.washington.edu/wobbrock/" target="_blank">
           <div style="width:50%;float:left;">
@@ -141,20 +141,19 @@
 
       <div class="four wide column centered">
         <h3 class="text text-spaced text-center">
-        <span>Posters Chair</span>
+        <span>Sponsorship Chair</span>
         </h3>
-        <a href="http://juhokim.com/" target="_blank">
+        <a href="http://yatani.jp/" target="_blank">
           <div>
-            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/juho-kim.jpg">
-            <h3 class="text-center">Juho Kim</h3>
+            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/koji-yatani.jpg">
+            <h3 class="text-center">Koji Yatani</h3>
           </div>
         </div>
       </a>
 
-
       <div class="six wide column centered">
         <h3 class="text text-spaced text-center">
-        <span>Demos Co-Chair</span>
+        <span>Demo Committee Chairs</span>
         </h3>
         <a href="http://www.chrisharrison.net/" target="_blank">
           <div style="width:50%;float:left;">
@@ -172,15 +171,34 @@
 
       <div class="four wide column centered">
         <h3 class="text text-spaced text-center">
-        <span>Sponsorship Chair</span>
+        <span>Poster Committe Chair</span>
         </h3>
-        <a href="http://yatani.jp/" target="_blank">
+        <a href="http://juhokim.com/" target="_blank">
           <div>
-            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/koji-yatani.jpg">
-            <h3 class="text-center">Koji Yatani</h3>
+            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/juho-kim.jpg">
+            <h3 class="text-center">Juho Kim</h3>
           </div>
         </div>
       </a>
+
+
+      <div class="seven wide column centered">
+        <h3 class="text text-spaced text-center">
+        <span>Treasurer and Local Arrangements</span>
+        </h3>
+        <a href="http://daisukesakamoto.jp/en/" target="_blank">
+          <div style="width:50%;float:left;">
+            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/daisuke-sakamoto.jpg">
+            <h3 class="text-center">Daisuke Sakamoto</h3>
+          </div>
+        </a>
+        <a href="http://masaogata.com/" target="_blank">
+          <div style="width:50%;float:left;">
+            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/masa-ogata.jpg">
+            <h3 class="text-center">Masa Ogata</h3>
+          </div>
+        </a>
+      </div>
 
       <div class="five wide column centered">
         <h3 class="text text-spaced text-center">
@@ -196,12 +214,42 @@
 
       <div class="five wide column centered">
         <h3 class="text text-spaced text-center">
-        <span>Student Innovation Contest</span>
+        <span>Student Innovation Contest Co-Chairs</span>
         </h3>
         <a href="http://stefaniemueller.org/" target="_blank">
-          <div>
+          <div style="width:50%;float:left;">
             <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/stefanie-mueller.jpg">
             <h3 class="text-center">Stefanie Mueller</h3>
+          </div>
+        </a>
+        <a href="http://web.media.mit.edu/~liningy/" target="_blank">
+          <div style="width:50%;float:left;">
+            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/lining-yao.jpg">
+            <h3 class="text-center">Lining Yao</h3>
+          </div>
+        </a>
+      </div>
+
+      <div class="five wide column centered">
+        <h3 class="text text-spaced text-center">
+        <span>Video Previews Chair</span>
+        </h3>
+        <a href="http://www.autodeskresearch.com/people/justin/" target="_blank">
+          <div>
+            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/justin-metejka.jpg">
+            <h3 class="text-center">Justin Matejka</h3>
+          </div>
+        </a>
+      </div>
+
+      <div class="five wide column centered">
+        <h3 class="text text-spaced text-center">
+        <span>Registration Chair</span>
+        </h3>
+        <a href="http://www.patrickbaudisch.com/" target="_blank">
+          <div>
+            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/jessica-cauchard.jpg">
+            <h3 class="text-center">Jessica Cauchard</h3>
           </div>
         </a>
       </div>
@@ -220,22 +268,16 @@
 
       <div class="five wide column centered">
         <h3 class="text text-spaced text-center">
-        <span>Treasurer</span>
-        </h3>
-        <a href="http://daisukesakamoto.jp/en/" target="_blank">
-          <div>
-            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/daisuke-sakamoto.jpg">
-            <h3 class="text-center">Daisuke Sakamoto</h3>
-          </div>
-        </a>
-      </div>
-
-      <div class="five wide column centered">
-        <h3 class="text text-spaced text-center">
         <span>Student Volunteer Chair</span>
         </h3>
+        <a href="http://www.davidlindlbauer.com/" target="_blank">
+          <div style="width:50%;float:left;">
+            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/david-lindlbauer.jpg">
+            <h3 class="text-center">David Lindlbauer</h3>
+          </div>
+        </a>
         <a href="http://junkato.jp/" target="_blank">
-          <div>
+          <div style="width:50%;float:left;">
             <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/jun-kato.jpg">
             <h3 class="text-center">Jun Kato</h3>
           </div>
@@ -254,25 +296,35 @@
         </a>
       </div>
 
-      <div class="seven wide column centered">
+      <div class="five wide column centered">
         <h3 class="text text-spaced text-center">
-        <span>Local Arrangements Chair</span>
+        <span>Conference Documentation Chair</span>
         </h3>
-        <a href="http://masaogata.com/" target="_blank">
+        <a href="http://www.gilbertbernstein.com/" target="_blank">
           <div style="width:50%;float:left;">
-            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/masa-ogata.jpg">
-            <h3 class="text-center">Masa Ogata</h3>
+            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/gilbert-bernstein.jpg">
+            <h3 class="text-center">Gilbert Bernstein</h3>
           </div>
         </a>
-        <a href="http://daisukesakamoto.jp/en/" target="_blank">
+        <a href="http://www.alexandraion.com/" target="_blank">
           <div style="width:50%;float:left;">
-            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/daisuke-sakamoto.jpg">
-            <h3 class="text-center">Daisuke Sakamoto</h3>
+            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/alexandra-ion.jpg">
+            <h3 class="text-center">Alexandra Ion</h3>
           </div>
         </a>
       </div>
 
-
+      <div class="five wide column centered">
+        <h3 class="text text-spaced text-center">
+        <span>Design and Signage Chair</span>
+        </h3>
+        <a href="http://www.xlab.sfc.keio.ac.jp/~kakehi/" target="_blank">
+          <div>
+            <img class="ui centered tiny circular image" src="<?php echo BASE_PATH; ?>/lib/img/people/yasuaki-kakehi.jpg">
+            <h3 class="text-center">Yasuaki Kakehi</h3>
+          </div>
+        </a>
+      </div>
       <!--
       <div class="four wide column centered">
         <h3 class="text text-spaced text-center">
