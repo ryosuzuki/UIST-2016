dfdd92578f94a169640d1352445bd1650a8309e7
Ryo Suzuki <ryosuzk@gmail.com>
2015-11-25 23:58:36 -0700

    Update

--- index.php
+++ index.php
@@ -14,12 +14,14 @@ if (strncasecmp(PHP_OS, 'WIN', 3) == 0) {
 $path = explode($delimiter, dirname(__FILE__));
 $path = "http://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']."/".array_pop($path);
 
+define("BASE_PATH", "http://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']);
+define("BASE_PATH", "http://uist-2016.herokuapp.com");
+
 $app = new \Slim\Slim(array(
   'mode' => 'development',
   'debug' => true,
   'templates.path' => __DIR__.'/Slim/Views'
 ));
-
 
 if ($_SERVER['SERVER_NAME'] == "uist.acm.org") {
   define("BASE_PATH", "http://uist.acm.org/uist2016");
@@ -29,7 +31,6 @@ if ($_SERVER['SERVER_NAME'] == "uist.acm.org") {
 }
 
 if ($_SERVER['SERVER_NAME'] == "uist-2016.herokuapp.com") {
-  define("BASE_PATH", "http://uist-2016.herokuapp.com");
   $app->add(new \Slim\Middleware\HttpBasicAuthentication([
     "secure" => false,
     "users" => [
@@ -37,13 +38,6 @@ if ($_SERVER['SERVER_NAME'] == "uist-2016.herokuapp.com") {
     ]
   ]));
 }
-
-if ($_SERVER['SERVER_NAME'] == "localhost") {
-  define("BASE_PATH", "http://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']);
-  // define("BASE_PATH", "http://uist-2016.herokuapp.com");
-}
-
-
 
 require './Slim/Config/routes.php';
 
