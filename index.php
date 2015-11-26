7a7495bee496814feb034a1c73cbda3e3d7dba47
Ryo Suzuki <ryosuzk@gmail.com>
2015-11-25 23:21:00 -0700

    Base path setting

--- index.php
+++ index.php
@@ -11,30 +11,27 @@ if (strncasecmp(PHP_OS, 'WIN', 3) == 0) {
   $delimiter = "/";
 }
 
+define("BASE_PATH", "http://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']);
+
 $path = explode($delimiter, dirname(__FILE__));
 $path = "http://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']."/".array_pop($path);
-
-if ($_SERVER['SERVER_NAME'] == 'uist.acm.org') {
-  define("BASE_PATH", "http://uist.acm.org/uist2016");
-} else {
-  define("BASE_PATH", "http://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']);
-}
-
-$app = new \Slim\Slim();
-$app->get('/', function () {
-  echo 'Hello World';
-});
-$app->get('/hoge', function () {
-  echo 'Hogee';
-});
 
 $app = new \Slim\Slim(array(
   'mode' => 'development',
   'debug' => true,
   'templates.path' => __DIR__.'/Slim/Views'
-  ));
+));
 
-if ($_SERVER['SERVER_NAME'] == 'uist-2016.herokuapp.com') {
+
+if ($_SERVER["SERVER_NAME"] == "uist.acm.org") {
+  define("BASE_PATH", "http://uist.acm.org/uist2016");
+  $app->hook('slim.before', function () use ($app) {
+    $app->view()->appendData(array('baseUrl' => 'http://uist.hosting.acm.org/uist2016'));
+  });
+}
+
+if ($_SERVER["SERVER_NAME"] == "uist-2016.herokuapp.com") {
+  define("BASE_PATH", "http://uist-2016.herokkuapp.com");
   $app->add(new \Slim\Middleware\HttpBasicAuthentication([
     "secure" => false,
     "users" => [
@@ -43,9 +40,6 @@ if ($_SERVER['SERVER_NAME'] == 'uist-2016.herokuapp.com') {
   ]));
 }
 
-$app->hook('slim.before', function () use ($app) {
-  $app->view()->appendData(array('baseUrl' => 'http://uist.hosting.acm.org/uist2016'));
-});
 
 require './Slim/Config/routes.php';
 
