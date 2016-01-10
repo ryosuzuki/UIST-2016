02cf95c4cbc2896fa678f3ff50f23a0a8dad700b
Ryo Suzuki <ryosuzk@gmail.com>
2016-01-10 12:02:55 +0900

    Update menu

--- Slim/Views/Partials/menu.php
+++ Slim/Views/Partials/menu.php
@@ -4,15 +4,19 @@
 <a href="<?php echo BASE_PATH; ?>/submission" class="item">
   Submission
 </a>
+<!--
 <a href="<?php echo BASE_PATH; ?>/attending" class="item">
   Attending
 </a>
+-->
 <a href="<?php echo BASE_PATH; ?>/organizers" class="item">
   Organizers
 </a>
+<!--
 <a href="<?php echo BASE_PATH; ?>/dates" class="item">
   Dates
 </a>
+-->
 <a href="<?php echo BASE_PATH; ?>/sponsors" class="item">
   Sponsors
 </a>
