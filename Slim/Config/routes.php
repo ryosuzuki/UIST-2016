f3f5e6d6d3dea27f0a00856e50e76409edccadd8
Ryo Suzuki <ryosuzk@gmail.com>
2016-02-04 14:05:56 -0500

    Change title dynamically and add open graph

--- Slim/Config/routes.php
+++ Slim/Config/routes.php
@@ -4,19 +4,19 @@ $app->get('/', function () use ($app) {
 });
 
 $app->get('/submission', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "submission.php", "category" => "submission"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "submission.php", "category" => "submission", "title" => "Submission"));
 });
 
 $app->get('/attending', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "attending.php", "category" => "attending"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "attending.php", "category" => "attending", "title" => "Attending"));
 });
 
 $app->get('/registration', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "registration.php", "category" => "registration"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "registration.php", "category" => "registration", "title" => "Registration"));
 });
 
 $app->get('/sponsors', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "sponsors.php", "category" => "sponsors"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "sponsors.php", "category" => "sponsors", "title" => "Sponsors"));
 });
 
 $app->get('/updates', function () use ($app) {
@@ -24,40 +24,40 @@ $app->get('/updates', function () use ($app) {
 });
 
 $app->get('/organizers', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "organizers.php", "category" => "organizers"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "organizers.php", "category" => "organizers", "title" => "Organizers"));
 });
 
 $app->get('/call', function () use ($app) {
   $app->render('Layouts/main.php', array("app" => $app, "view" => "call/call-papers.php", "category" => "call"));
 });
 $app->get('/papers', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/call-papers.php", "category" => "call"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/call-papers.php", "category" => "call", "title" => "Papers & Notes"));
 });
 $app->get('/demos', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/call-demos.php", "category" => "call"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/call-demos.php", "category" => "call", "title" => "Demos"));
 });
 $app->get('/volunteers', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/call-sv.php", "category" => "call"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/call-sv.php", "category" => "call", "title" => "Student Volunteers"));
 });
 $app->get('/posters', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/call-posters.php", "category" => "call"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/call-posters.php", "category" => "call", "title" => "Posters"));
 });
 $app->get('/doctoral', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/call-doctoral.php", "category" => "call"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/call-doctoral.php", "category" => "call", "title" => "Doctoral Symposium"));
 });
 
 $app->get('/author-guide', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/author-guide.php", "category" => "call"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/author-guide.php", "category" => "call", "title" => "Author's Guide"));
 });
 $app->get('/author-advice', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/author-advice.php", "category" => "call"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/author-advice.php", "category" => "call", "title" => "Advice for Submission"));
 });
 $app->get('/video-guide', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/video-guide.php", "category" => "call"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/video-guide.php", "category" => "call", "title" => "Video Guide"));
 });
 
 $app->get('/video-previews', function () use ($app) {
-  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/video-previews.php", "category" => "call"));
+  $app->render('Layouts/main.php', array("app" => $app, "view" => "call/video-previews.php", "category" => "call", "title" => "Video Previews"));
 });
 $app->get('/video-previews-detail', function () use ($app) {
   $app->render('Layouts/main.php', array("app" => $app, "view" => "call/video-previews-detail.php", "category" => "call"));
--- Slim/Views/Layouts/main.php
+++ Slim/Views/Layouts/main.php
@@ -4,7 +4,30 @@
     <meta charset="UTF-8" name="viewport" content="width=device-width, user-scalable=no" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <title>UIST 2016: 29th ACM UIST Symposium</title>
+    <?php
+    if (isset($title)) {
+      $title = $title . " - UIST 2016: 29th ACM User Interface Software and Technology Symposium";
+    } else {
+      $title = "UIST 2016: 29th ACM User Interface Software and Technology Symposium";
+    }
+    $description = "The ACM Symposium on User Interface Software and Technology (UIST) is the premier forum for innovations in human-computer interfaces. Sponsored by ACM special interest groups on computer-human interaction (SIGCHI) and computer graphics (SIGGRAPH), UIST brings together people from diverse areas including graphical & web user interfaces, tangible & ubiquitous computing, virtual & augmented reality, multimedia, new input & output devices, and CSCW. The intimate size and intensive program make UIST an ideal opportunity to exchange research results and ideas. Join us in Tokyo!"
+    ?>
+    <title><?php echo $title ?></title>
+    <meta property="og:type" content="website">
+    <meta property="og:title" content='<?php echo $title ?>'>
+    <meta property="og:image" content='<?php echo BASE_PATH; ?>/lib/img/cover.png'>
+    <!-- <meta property="og:url" content="http://uist.acm.org/uist2016/"> -->
+    <meta property="og:description" content="<?php echo $description ?>">
+    <meta property="og:site_name" content="UIST 2016">
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@acmuist">
+    <meta name="twitter:creator" content="@acmuist">
+    <!-- <meta property="og:url" content=""> -->
+    <meta property="og:title" content="<?php echo $title ?>">
+    <meta property="og:description" content="<?php echo $description ?>">
+    <meta property="og:image" content='<?php echo BASE_PATH; ?>/lib/img/cover.png'>
+
     <link rel="icon" type="image/png" href='<?php echo BASE_PATH; ?>/lib/img/icon-acm.gif'>
     <link rel='stylesheet' type='text/css' href='<?php echo BASE_PATH; ?>/bower_components/semantic-ui/dist/semantic.css' />
     <link rel='stylesheet' type='text/css' href='<?php echo BASE_PATH; ?>/bower_components/fontawesome/css/font-awesome.css' />
