02674ad1405fa7e487ca9bfaaa6530e659aa7e37
Ryo Suzuki <ryosuzk@gmail.com>
2016-03-15 23:21:50 -0600

    Add program committee

---
+++ Slim/Views/Partials/committee.php
@@ -0,0 +1,323 @@
+<div class="ui stackable grid">
+  <section id="committee" class="twelve wide column centered">
+    <h2 class="ui horizontal divider header">Program Committee</h2>
+    <div class="ui stackable grid detail">
+      <div class="fourteen wide column centered">
+        <table class="ui fixed single line table">
+          <thead class="text">
+            <tr>
+              <th>Name</th>
+              <th>Affiliation</th>
+            </tr>
+          </thead>
+          <tbody class="text">
+            <tr>
+              <td class="name">
+                <a href="https://aquigley.host.cs.st-andrews.ac.uk/" target="_blank">
+                Arron Quigley
+                </a>
+              </td>
+              <td class="affiliation">University of St. Andrews</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="https://www.lri.fr/~appert/website/index.html" target="_blank">
+                Caroline Appert
+                </a>
+              </td>
+              <td class="affiliation">Université Paris-Sud</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://danielashbrook.com/" target="_blank">
+                Daniel Ashbrook
+                </a>
+              </td>
+              <td class="affiliation">Rochester Institute of Technology</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://research.microsoft.com/en-us/people/davidkim/" target="_blank">
+                David Kim
+                </a>
+              </td>
+              <td class="affiliation">Microsoft Research Cambridge</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="https://homes.cs.washington.edu/~jfogarty/" target="_blank">
+                James Fogarty
+                </a>
+              </td>
+              <td class="affiliation">University of Washington</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://www.juew.org/" target="_blank">
+                Jue Wang
+                </a>
+              </td>
+              <td class="affiliation">Adobe Research</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://www.dgp.toronto.edu/~karan/" target="_blank">
+                Karan Singh
+                </a>
+              </td>
+              <td class="affiliation">University of Toronto</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://research.microsoft.com/en-us/um/people/kenh/" target="_blank">
+                Ken Hinckley
+                </a>
+              </td>
+              <td class="affiliation">Microsoft Research</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://www.mansci.uwaterloo.ca/~pchilana/" target="_blank">
+                Parmit Chilana
+                </a>
+              </td>
+              <td class="affiliation">University of Waterloo</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://pokristensson.com/" target="_blank">
+                Per Ola Kristensson
+                </a>
+              </td>
+              <td class="affiliation">Cambridge University (U.K.)</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://www.eed.usv.ro/~vatavu/" target="_blank">
+                Radu Daniel-Vatavu
+                </a>
+              </td>
+              <td class="affiliation">University Stefan cel Mare of Suceava</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://ranjithakumar.net/" target="_blank">
+                Ranjitha Kumar
+                </a>
+              </td>
+              <td class="affiliation">University of Illinois at Urbana-Champaign</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://www.cs.cmu.edu/~hudson/" target="_blank">
+                Scott Hudson
+                </a>
+              </td>
+              <td class="affiliation">Carnegie Mellon University</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://research.microsoft.com/en-us/um/people/ssaponas/" target="_blank">
+                Scott Saponas
+                </a>
+              </td>
+              <td class="affiliation">Microsoft Research</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="https://profiles.stanford.edu/sean-follmer" target="_blank">
+                Sean Follmer
+                </a>
+              </td>
+              <td class="affiliation">Stanford University</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://www.sebastianboring.com/" target="_blank">
+                Sebastian Boring
+                </a>
+              </td>
+              <td class="affiliation">University of Copenhagen</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://shaunkane.info/" target="_blank">
+                Shaun Kane
+                </a>
+              </td>
+              <td class="affiliation">University of Colorado Boulder</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://www.shengdongzhao.com/" target="_blank">
+                Shengdong (Shen) Zhao
+                </a>
+              </td>
+              <td class="affiliation">National University of Singapore</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://stefaniemueller.org/" target="_blank">
+                Stefanie Muller
+                </a>
+              </td>
+              <td class="affiliation">Hasso Plattner Institute</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://caoxiang.net/" target="_blank">
+                Xiang Cao
+                </a>
+              </td>
+              <td class="affiliation">Xiaoxiaoniu Creative Technologies</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://www.cs.usask.ca/faculty/gutwin/" target="_blank">
+                Carl Gutwin
+                </a>
+              </td>
+              <td class="affiliation">University of Saskatchewan</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://makeabilities.org/people/jen-mankoff/" target="_blank">
+                Jennifer Mankoff
+                </a>
+              </td>
+              <td class="affiliation">Carnegie Mellon University</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="https://www.lri.fr/~anab/" target="_blank">
+                Anastasia Bezerianos
+                </a>
+              </td>
+              <td class="affiliation">LRI-Université Paris-Sud</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://wslasecki.com/" target="_blank">
+                Walter Lasecki
+                </a>
+              </td>
+              <td class="affiliation">University of Michigan</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://dangoldman.net/" target="_blank">
+                Dan Goldman
+                </a>
+              </td>
+              <td class="affiliation">Google</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://cristal.univ-lille.fr/~casiez/" target="_blank">
+                Géry Casiez
+                </a>
+              </td>
+              <td class="affiliation">University of Lille</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="" target="_blank">
+                Rubaiat Habib Kazi
+                </a>
+              </td>
+              <td class="affiliation">Autodesk Research</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://rubaiathabib.me/" target="_blank">
+                Fanny Chevalier
+                </a>
+              </td>
+              <td class="affiliation">University of Toronto</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://www.dgp.toronto.edu/~shbae/" target="_blank">
+                Seok-Hyung Bae
+                </a>
+              </td>
+              <td class="affiliation">KAIST</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="https://cs.uwaterloo.ca/~lank/" target="_blank">
+                Edward Lank
+                </a>
+              </td>
+              <td class="affiliation">University of Waterloo</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="https://autodeskresearch.com/people/justin-matejka" target="_blank">
+                Justin Matejka
+                </a>
+              </td>
+              <td class="affiliation">Autodesk Research</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="https://www.cs.umanitoba.ca/~bunt/" target="_blank">
+                Andrea Bunt
+                </a>
+              </td>
+              <td class="affiliation">University of Manitoba</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://research.microsoft.com/en-us/um/people/benko/" target="_blank">
+                Hrvoje Benko
+                </a>
+              </td>
+              <td class="affiliation">Microsoft Research</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://ait.ethz.ch/people/hilliges/" target="_blank">
+                Otmar Hilliges
+                </a>
+              </td>
+              <td class="affiliation">ETH Zurich</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://people.csail.mit.edu/rcm/" target="_blank">
+                Rob Miller
+                </a>
+              </td>
+              <td class="affiliation">MIT</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://www.xiaojunbi.com/" target="_blank">
+                Xiaojun Bi
+                </a>
+              </td>
+              <td class="affiliation">Google Research</td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://www.adobe.com/technology/people/san-francisco/joel-brandt.html" target="_blank">
+                Joel Brandt
+                </a>
+              </td>
+              <td class="affiliation">Adobe Research </td>
+            </tr>
+            <tr>
+              <td class="name">
+                <a href="http://yutasugiura.com/index-e.html" target="_blank">
+                Yuta Sugiura
+                </a>
+              </td>
+              <td class="affiliation">AIST, Japan</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
+</div>
--- Slim/Views/organizers.php
+++ Slim/Views/organizers.php
@@ -1,35 +1,3 @@
 <?php $app->render('Partials/organizers.php'); ?>
 
-<div class="ui stackable grid">
-  <section class="twelve wide column centered">
-    <h2 class="ui horizontal divider header">Program Committee</h2>
-    <div class="ui stackable grid detail">
-      <div class="twelve wide column centered">
-        <table class="ui fixed single line table">
-          <thead class="text">
-            <tr>
-              <th>Name</th>
-              <th>Affiliation</th>
-            </tr>
-          </thead>
-          <tbody class="text">
-            <tr>
-              <td>TBA</td>
-              <td>TBA</td>
-            </tr>
-          <!--
-            <tr>
-              <td>Takeo Igarashi</td>
-              <td>The University of Tokyo</td>
-            </tr>
-            <tr>
-              <td>Jun Rekimoto</td>
-              <td>The University of Tokyo</td>
-            </tr>
-          -->
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </section>
-</div>
+<?php $app->render('Partials/committee.php'); ?>
