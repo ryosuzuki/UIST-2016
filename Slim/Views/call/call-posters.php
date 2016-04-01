085f4a13b57c38e25ff494ab6619dc9a1cdd2f6a
Ryo Suzuki <ryosuzk@gmail.com>
2016-04-01 10:15:23 -0600

    Add Call for Posters

--- Slim/Views/call/call-posters.php
+++ Slim/Views/call/call-posters.php
@@ -1,20 +1,53 @@
-<div class="section about"><div class="blk-1">
 
+<div class="ui stackable grid">
+  <section id="call" class="twelve wide column centered">
+  <h2 class="ui horizontal divider header">Call for Posters</h2>
+  <div class="ui stackable grid detail">
+  <div class="fourteen wide column centered">
 
-<h1 id="call-for-posters">Call for Posters</h1>
-<p>Posters provide an interactive forum in which authors can present their work to conference attendees during special poster sessions. Posters are an opportunity to describe new work or work that is still in progress, and will be more lightly reviewed than papers.  </p>
-<p>All poster submissions must include a two-page &quot;abstract&quot; and a full size poster design. <b>Note that poster submissions, unlike paper submissions, are not anonymous.</b> The abstract describes the research problem, contribution, and value to UIST attendees. The abstract must be formatted using the <em>SIGCHI Papers Format</em> (use the <a href="http://www.sigchi.org/publications/chipubform/sigchi-papers-word-template/view">Word Template</a> or <a href="http://www.sigchi.org/publications/chipubform/sigchi-papers-latex-template/view">Latex Template</a>) and submitted as a PDF. The poster design must be less than 30 x 40 inches (76 x 101 cm), either portrait or landscape orientation, and submitted as a PDF. The <a href="http://www.acm.org/uist/uist2005/posterexamplegallery.html">UIST 2005 Poster Example Gallery</a> has ideas and guidelines for poster designs.  Authors are also encouraged to submit an optional video no more than three minutes long, see the <a href="/video-guide">video guide</a> for acceptable formats.
-The poster abstract, poster design, and optional video must be submitted electronically to <a href="http://www.precisionconference.com/~sigchi">Precision Conference</a> before the submission date below. The total size for all files must be less than 50 MB. See the <a href="video-guide">video guide</a>, the <a href="author-guide">author guide</a>, and <a href="call">general submission information</a> for more details about preparing your submission.  </p>
-<p>All accepted posters will be displayed during a portion of the conference. At least one author is requested to stand by their poster during poster sessions to speak with conference attendees. Accepted poster abstracts will be published as adjunct proceedings in the ACM Digital Library and distributed in digital form to conference attendees. Authors can make accepted poster designs and videos available at their discretion.</p>
-<p>Incomplete or incorrect submission content or formats will be desk rejected. Late submissions are not permitted, the deadline below is final. Do not submit the same work to both the demo and poster tracks. Double submissions will only be considered as a demo submission and will be automatically rejected from the poster track.</p>
-<h3 id="relevant-dates-">Relevant dates:</h3>
-<ul>
-<li><b>July 9:</b> Submissions due by 17:00 PDT.</li>
-<li><b>August 13:</b> Notification of acceptance decisions.</li>
-<li><b>August 23:</b> Camera ready versions due.  </li>
-</ul>
-<p>Posters Chairs (<a href="mailto:posters@uist.org">posters@uist.org</a>): Juho Kim, MIT, USA; Daniel Vogel, University of Waterloo, Canada</p>
+  <h1>Call for Posters</h1>
+  <h2>
+    PAPERS (Deadline: Wednesday April 13, 2016 12:00 PDT)
+  </h2>
 
+  <div class="text">
+    Posters provide an interactive forum in which authors can present their work to conference attendees during special poster sessions. Posters are an opportunity to describe new work or work that is still in progress, and will be more lightly reviewed than papers. The posters track welcomes innovative research, as well as innovative ways of doing research.
+    <br>
+    <br>
+    All poster submissions must include a two-page “abstract” and a full size poster design. Note that poster submissions, unlike paper submissions, are not anonymous. The abstract describes the research problem, contribution, and value to UIST attendees. The abstract must be formatted using the SIGCHI Papers Format (use the <a href="http://www.sigchi.org/publications/chipubform/sigchi-papers-word-template/view" target="_blank">Word Template</a> or <a href="http://www.sigchi.org/publications/chipubform/sigchi-papers-latex-template/view" target="_blank">Latex Template</a>) and submitted as a PDF. The poster design must be less than 30 x 40 inches (76 × 101 cm), either portrait or landscape orientation, and submitted as a PDF. The <a href="https://uist.acm.org/uist2005/posterexamplegallery.html" target="_blank">UIST 2005 Poster Example Gallery</a> has ideas and guidelines for poster designs.  Authors are also encouraged to submit an optional video no more than three minutes long, see the <a href="https://uist.acm.org/uist2016/video-guide" target="_blank">video guide</a> for acceptable formats.
+    <br>
+    <br>
+    The poster abstract, poster design, and optional video must be submitted electronically to <a href="http://precisionconference.com/~sigchi" target="_blank">Precision Conference</a> before the submission date below. The total size for all files must be less than 50 MB. See the <a href="https://uist.acm.org/uist2016/video-guide" target="_blank">video guide</a>, the <a href="https://uist.acm.org/uist2016/author-guide" target="_blank">author guide</a>, and <a href="https://uist.acm.org/uist2016/author-advice" target="_blank">general submission information</a> for more details about preparing your submission.
+    <br>
+    <br>
+    All accepted posters will be displayed during a portion of the conference. At least one author is requested to stand by their poster during poster sessions to speak with conference attendees. Accepted poster abstracts will be published as adjunct proceedings in the ACM Digital Library and distributed in digital form to conference attendees. Authors can make accepted poster designs and videos available at their discretion.
+    <br>
+    <br>
+    Incomplete or incorrect submission content or formats will be desk rejected. Late submissions are not permitted, and the deadline below is final. Do not submit the same work to both the demo and poster tracks. Double submissions will only be considered as a demo submission and will be automatically rejected from the poster track.
+  </div>
+  <h2>
+    Relevant dates:
+  </h2>
+  <div class="text">
+    <ul>
+      <li>
+        July 7, 2016: Submissions due by 12:00 PDT.
+      </li>
+      <li>
+        August 12, 2016: Notification of acceptance decisions.
+      </li>
+      <li>
+        August 10, 2016: Camera ready versions due.
+      </li>
+    </ul>
+  </div>
 
+  <h2>
+    Posters Chairs (<a href="mailto:posters@uist.org">posters@uist.org</a>)
+  </h2>
+  <div class="text">
+    Juho Kim (KAIST, Korea), Stefanie Mueller (HPI, Germany)
+  </div>
+</section>
+</div>
 
-</div></div>
