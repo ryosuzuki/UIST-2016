1b2571d9aaad1ac4a22f4dc7e3e5586c9809024e
Ryo Suzuki <ryosuzk@gmail.com>
2016-04-02 17:29:57 -0600

    Add student volunteers

--- Slim/Views/call/call-sv.php
+++ Slim/Views/call/call-sv.php
@@ -5,57 +5,12 @@
   <div class="ui stackable grid detail">
   <div class="fourteen wide column centered">
 
-  <h1>Posters (Deadline: </h1>
+  <h1>Student Volunteers</h1>
   <h2>
-    PAPERS (Deadline: July 7, 2016 12:00 PDT)
-
-    Wednesday April 13, 2016 12:00 PDT)
+    SVs (Deadline: July 25, 2016 17:00 PDT)
   </h2>
 
   <div class="text">
-    Posters provide an interactive forum in which authors can present their work to conference attendees during special poster sessions. Posters are an opportunity to describe new work or work that is still in progress, and will be more lightly reviewed than papers. The posters track welcomes innovative research, as well as innovative ways of doing research.
-    <br>
-    <br>
-    All poster submissions must include a two-page “abstract” and a full size poster design. Note that poster submissions, unlike paper submissions, are not anonymous. The abstract describes the research problem, contribution, and value to UIST attendees. The abstract must be formatted using the SIGCHI Papers Format (use the <a href="http://www.sigchi.org/publications/chipubform/sigchi-papers-word-template/view" target="_blank">Word Template</a> or <a href="http://www.sigchi.org/publications/chipubform/sigchi-papers-latex-template/view" target="_blank">Latex Template</a>) and submitted as a PDF. The poster design must be less than 30 x 40 inches (76 × 101 cm), either portrait or landscape orientation, and submitted as a PDF. The <a href="https://uist.acm.org/uist2005/posterexamplegallery.html" target="_blank">UIST 2005 Poster Example Gallery</a> has ideas and guidelines for poster designs.  Authors are also encouraged to submit an optional video no more than three minutes long, see the <a href="https://uist.acm.org/uist2016/video-guide" target="_blank">video guide</a> for acceptable formats.
-    <br>
-    <br>
-    The poster abstract, poster design, and optional video must be submitted electronically to <a href="http://precisionconference.com/~sigchi" target="_blank">Precision Conference</a> before the submission date below. The total size for all files must be less than 50 MB. See the <a href="https://uist.acm.org/uist2016/video-guide" target="_blank">video guide</a>, the <a href="https://uist.acm.org/uist2016/author-guide" target="_blank">author guide</a>, and <a href="https://uist.acm.org/uist2016/author-advice" target="_blank">general submission information</a> for more details about preparing your submission.
-    <br>
-    <br>
-    All accepted posters will be displayed during a portion of the conference. At least one author is requested to stand by their poster during poster sessions to speak with conference attendees. Accepted poster abstracts will be published as adjunct proceedings in the ACM Digital Library and distributed in digital form to conference attendees. Authors can make accepted poster designs and videos available at their discretion.
-    <br>
-    <br>
-    Incomplete or incorrect submission content or formats will be desk rejected. Late submissions are not permitted, and the deadline below is final. Do not submit the same work to both the demo and poster tracks. Double submissions will only be considered as a demo submission and will be automatically rejected from the poster track.
-  </div>
-  <h2>
-    Relevant dates:
-  </h2>
-  <div class="text">
-    <ul>
-      <li>
-        <b>July 7, 2016:</b> Submissions due by 12:00 PDT.
-      </li>
-      <li>
-        <b>August 12, 2016:</b> Notification of acceptance decisions.
-      </li>
-      <li>
-        <b>August 10, 2016:</b> Camera ready versions due.
-      </li>
-    </ul>
-  </div>
-
-  <h2>
-    Posters Chairs (<a href="mailto:posters@uist.org">posters@uist.org</a>)
-  </h2>
-  <div class="text">
-    Juho Kim (KAIST, Korea), Stefanie Mueller (HPI, Germany)
-  </div>
-</section>
-</div>
-
-
-Student Volunteers (Deadline: July 25, 2016 17:00 PDT)
-
 Application Instructions:
 
 To apply to be a student volunteer, please sign up at the UIST SV portal.
@@ -108,61 +63,31 @@ team up with other student volunteers for sharing rooms and we will provide a li
 
 affordable room rates.
 
-Relevant Dates:
-
- July 25, 2016: Application Deadline (by 17:00 PDT)
-
- July 31, 2016: Application Notified (by 17:00 PDT)
-
-Student Volunteer Chairs: (sv@uist.org)
-
-David Lindlbauer (TU Berlin, Germany)
-
-Jun Kato (National Institute of Advanced Industrial Science and Technology, Japan)
 
 
 
 
+    </div>
+  <h2>
+    Relevant dates:
+  </h2>
+  <div class="text">
+    <ul>
+      <li>
+        <b>July 25, 2016:</b> Application Deadline by 17:00 PDT.
+      </li>
+      <li>
+        <b>July 31, 2016:</b> Application Notified by 17:00 PDT.
+      </li>
+    </ul>
+  </div>
 
+  <h2>
+    Student Volunteer Chairs (<a href="mailto:sv@uist.org">sv@uist.org</a>)
+  </h2>
+  <div class="text">
+    David Lindlbauer (TU Berlin, Germany), Jun Kato (National Institute of Advanced Industrial Science and Technology, Japan)
+  </div>
+</section>
+</div>
 
-
-
-
-
-<div class="section about"><div class="blk-1">
-
-
-<h1 id="student-volunteers">Student Volunteers</h1>
-<h3 id="deadline-august-10-2015-17-00-pdt">Deadline: August 10, 2015 17:00 PDT</h3>
-<h3 id="application-instructions">Application Instructions</h3>
-<p>To apply to be a student volunteer, please sign up at the <a href="http://chisv.org/uist2015/news">UIST SV portal</a>. </p>
-<h3 id="questions-">Questions? </h3>
-<p>If you have any questions/problems, please contact the chairs at <a href="mailto:sv@uist.org">sv@uist.org</a>.</p>
-<h3 id="benefits-and-duties">Benefits and Duties</h3>
-<p>If you are a UIST Student Volunteer, you get:</p>
-<ul>
-<li>Free conference registration</li>
-<li>Free T-shirt</li>
-<li>A great SV party with free dinner </li>
-<li>A chance to attend the premier forum for innovations in human-computer interfaces</li>
-</ul>
-<p>Student volunteers will be required to help set up and perform needed work during the conference. Duties include: bag stuffing, registration desk, AV assistance, poster and demo setup, badge checking, break monitoring, etc. Volunteers will also need to be available during the conference if tasks come up. SVs usually get to attend most of the sessions, even when working. There will be approximately 14 scheduled hours per SV. </p>
-<h3 id="arriving-and-leaving">Arriving and Leaving</h3>
-<p>SVs need to be available to help throughout the conference. You will need to arrive on or before Saturday afternoon (Nov. 7) for the SV orientation and should not leave the conference venue until sessions end on Wednesday (Nov. 11). Also, we strongly encourage you to put off leaving until as late as you possibly can, because it&#39;s traditional to have a small party for student volunteers on Wednesday evening. The time and place for this party will be announced when we know all the student volunteers&#39; travel plans.</p>
-<h3 id="work-schedules">Work Schedules</h3>
-<p>To give you an idea of what to expect, you can see the <a href="http://www.acm.org/uist/uist2008/uist06schedule.html">UIST 2006 Student Volunteer Schedule</a>.</p>
-<h3 id="accommodation">Accommodation</h3>
-<p>We will not be able to provide free accommodation, but we will help you team up with other student volunteers for sharing rooms and we will provide a list of hotels with affordable room rates. </p>
-<h3 id="relevant-dates-">Relevant Dates:</h3>
-<ul>
-<li><b>August 10, 2015:</b> Application Deadline (by 17:00 PDT)</li>
-<li><b>August 15, 2015:</b> Application Notified (by 17:00 PDT)</li>
-</ul>
-<h3 id="student-volunteer-chairs-sv-uist-org-mailto-sv-uist-org-">Student Volunteer Chairs: (<a href="mailto:sv@uist.org">sv@uist.org</a>) </h3>
-<ul>
-<li>Christian Rendl (University of Applied Sciences Upper Austria) </li>
-<li>Berto Gonzalez (UNC at Charlotte)</li>
-</ul>
-
-
-</div></div>
