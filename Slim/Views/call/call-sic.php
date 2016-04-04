4620c54da4c069c05718f324347cd13db250c4bf
Ryo Suzuki <ryosuzk@gmail.com>
2016-04-03 20:34:12 -0600

    Start preparing student innovation contest

--- Slim/Views/call/call-sic.php
+++ Slim/Views/call/call-sic.php
@@ -1,3 +1,109 @@
+
+<div class="ui stackable grid">
+  <section id="call" class="twelve wide column centered">
+  <h2 class="ui horizontal divider header">Call for Posters</h2>
+  <div class="ui stackable grid detail">
+  <div class="fourteen wide column centered">
+
+  <h1>Call for Posters</h1>
+  <h2>POSTERS (Deadline: Thursday July 7, 2016 12:00 PDT)</h2>
+  <div class="text">
+    Posters provide an interactive forum in which authors can present their work to conference attendees during special poster sessions. Posters are an opportunity to describe new work or work that is still in progress, and will be more lightly reviewed than papers. The posters track welcomes innovative research, as well as innovative ways of doing research.
+    <br>
+    <br>
+    All poster submissions must include a two-page “abstract” and a full size poster design. Note that poster submissions, unlike paper submissions, are not anonymous. The abstract describes the research problem, contribution, and value to UIST attendees. The abstract must be formatted using the SIGCHI Papers Format (use the <a href="http://www.sigchi.org/publications/chipubform/sigchi-papers-word-template/view" target="_blank">Word Template</a> or <a href="http://www.sigchi.org/publications/chipubform/sigchi-papers-latex-template/view" target="_blank">Latex Template</a>) and submitted as a PDF. The poster design must be less than 30 x 40 inches (76 × 101 cm), either portrait or landscape orientation, and submitted as a PDF. The <a href="https://uist.acm.org/uist2005/posterexamplegallery.html" target="_blank">UIST 2005 Poster Example Gallery</a> has ideas and guidelines for poster designs.  Authors are also encouraged to submit an optional video no more than three minutes long, see the <a href="https://uist.acm.org/uist2016/video-guide" target="_blank">video guide</a> for acceptable formats.
+    <br>
+    <br>
+    The poster abstract, poster design, and optional video must be submitted electronically to <a href="http://precisionconference.com/~sigchi" target="_blank">Precision Conference</a> before the submission date below. The total size for all files must be less than 50 MB. See the <a href="https://uist.acm.org/uist2016/video-guide" target="_blank">video guide</a>, the <a href="https://uist.acm.org/uist2016/author-guide" target="_blank">author guide</a>, and <a href="https://uist.acm.org/uist2016/author-advice" target="_blank">general submission information</a> for more details about preparing your submission.
+    <br>
+    <br>
+    All accepted posters will be displayed during a portion of the conference. At least one author is requested to stand by their poster during poster sessions to speak with conference attendees. Accepted poster abstracts will be published as adjunct proceedings in the ACM Digital Library and distributed in digital form to conference attendees. Authors can make accepted poster designs and videos available at their discretion.
+    <br>
+    <br>
+    Incomplete or incorrect submission content or formats will be desk rejected. Late submissions are not permitted, and the deadline below is final. Do not submit the same work to both the demo and poster tracks. Double submissions will only be considered as a demo submission and will be automatically rejected from the poster track.
+  </div>
+  <h2>
+    Relevant dates:
+  </h2>
+  <div class="text">
+    <ul>
+      <li>
+        <b>July 7, 2016:</b> Submissions due by 12:00 PDT.
+      </li>
+      <li>
+        <b>August 10, 2016:</b> Notification of acceptance decisions.
+      </li>
+      <li>
+        <b>August 10, 2016:</b> Camera ready versions due.
+      </li>
+    </ul>
+  </div>
+
+  <h2>
+    Posters Chairs (<a href="mailto:posters@uist.org">posters@uist.org</a>)
+  </h2>
+  <div class="text">
+    Juho Kim (KAIST, Korea), Stefanie Mueller (HPI, Germany)
+  </div>
+</section>
+</div>
+
+
+
+
+Student Innovation Contest
+Welcome to the eighth annual UIST Student Innovation Contest (SIC) taking place in Tokyo!
+TL;DR: The next quantum leap in human-computer interfaces is making the feeling the virtual world. In this contest you will use a simple, yet powerful technology borrowed from rehabilitation medicine, to make this possible: Electrical Muscle Stimulation. Using EMS you can create strong physical sensations such as forces with wearable hardware. Join the UIST SIC and be one of the first to develop new exciting haptic sensations for applications such as virtual and augmented reality! Win fabulous prizes! Sign up at http://tinyurl.com/uistsic2016 by August 1st of 2016.
+In the UIST Student Innovation Contest student teams create intriguing novel interfaces using exciting hardware kits that we provide, this year it is: Electrical Muscle Stimulation (EMS). It’s your opportunity to shine and impress the world with your innovations! Contestants will demo their creations during the demo reception at the conference in Tokyo Japan; and the winners will be announced at the UIST banquet. A jury of UIST celebrities will select two winners in the Most Creative Interface and Best Implementation categories. On top of that, conference attendees will get a chance to vote for their favorite teams in the People's Choice category.
+Important dates
+student contest registration deadline for all teams : August 1
+organizers evaluate submissions and select accepted teams : August 1-2
+notification of team selection sent out : August 3
+hardware kits shipped to accepted teams (as soon as possible after one team member is registered to the conference) : August 4
+contest development period with muscle-stimulation hardware : August 4 - October 17
+teams demo at contest reception during UIST conference (Tokyo) : October 17
+CONTEST ANNOUNCEMENT
+For 2016, the UIST Student Innovation Contest takes on a very timely subject which is the next frontier in virtual and augmented reality: feeling the virtual (haptics)! You will help create a world in which virtual interfaces are no longer just wireframes without mass... you can use electrical muscle stimulation to create physical forces from the virtual world and... you can even make it mobile/wearable so that these interfaces are with us everywhere!
+Each team will receive a kit of parts including an arduino nano, the medically-safe muscle stimulator, electrodes and our usb/bluetooth control board. Using these parts, teams will be able to build virtual experiences, devices, interfaces that demonstrate how to make the virtual physical. We will also be providing you with simple examples on how to get started using Arduino, Unity3D and Processing. All teams are encouraged to open source their efforts, and even build on each other’s work. To get you started easily, we will provide you, besides all the hardware, video tutorials and coaching sessions so you know how to setup everything and debug any questions you might have!
+Demo applications and more ideas to build
+The simplest demo you can build is one that renders a virtual event into a physical event, for instance a mobile phone game in which you actually steer an airplane against the wind (spoiler alert: we've done this one, check out the video here). Because using EMS makes interfaces more physical, when the wind comes one of the player's wrists contracts (using the muscle stimulation) and turns the airplane into an undesired direction, thus the player must turn against the force-feedback and steer the airplane into safety! What other futuristic mobile apps can you make that use EMS? A more compelling demo that you can build takes place in Virtual Reality, what could you improve in the experience of virtual reality if the user could feel forces? Check this video example for inspiration, here you see Virtual Reality sport simulators such as boxing and baseball that actually deliver a force. Here we combined the EMS bracelet with a solenoid... what other novel techniques and devices can you combine EMS with? You can even combine it with everyday items and make them smarter: what if while the user is manipulating an object you could actuate and change the way they physically handle the object? Check this video for inspiration in which we made a spray can shake by itself. Think outside the box and create novel interfaces that take advantage of these physical sensations for augmented or virtual reality or maybe you don't even need screens! The more exciting your demo gets the higher are the chances for the prize committee to be impressed.
+REGISTRATION PROCESS
+Here's the 6 easy steps to participate:
+write a 100-word description of your idea
+prepare a link to a picture/drawing/storyboard or sketch of your demo idea
+register for the UIST contest by fulling out the following form by August 1st: http://tinyurl.com/uistsic2016
+await notification and further instructions. Your team will be notified of our decision via email.
+for accepted teams, one of your team members MUST register for the UIST conference and pay the registration fee or inform us that you have been accepted as a student volunteer.
+once we've confirmed your UIST conference registration, we will ship you the hardware kit and arrange a skype meeting with you to get you started!
+Still have questions or confused? Drop us an email at: studentinnovation@uist.org
+CONTEST RULES AND DETAILS
+pre-selection criteria: organizers will select a maximum of 20 teams, judging your demo descriptions and sketches by the following criteria: complexity of idea, time and resources required to build, and creativity of idea.
+team size: the minimum team size is 1, the maximum is 4
+team composition: an individual student cannot be a part of multiple teams
+eligibility requirement: you MUST be a student to participate in this contest. Students of all levels (high school, bachelor's, master's, PhD) are encouraged to participate. International students are welcome.
+registration requirement: you must register for the contest and submit your idea and image link. If your team is accepted, at least one of your team members MUST register for the conference (or be accepted as a student volunteer). This is for three reasons: 1) you have to demo your idea, 2) you have to claim your prizes, and 3) UIST is an awesome conference!
+demo ideas: during the contest voting period, teams will be allowed to demo one idea. Different demos of the same idea are permitted.
+demo code: We encourage you to open-source your code. The organizers will create a github organization where all teams will be expected to post their code (don’t worry, we will not judge you on your code!).
+demo safety: We will provide you guides to follow and a skype meeting to help you get started. It is mandatory to read our guide on EMS and follow always the correct procedure. Please be careful!
+hardware provided: Muscle stimulation device (rehabilitation device), control board, arduino nano, electrodes.
+hardware modifications: No not modify the control board. Discuss any hardware modification with us first. Also, there is no need to modify the software of the control board, if you need some new feature discuss it with us.
+additional equipment: teams must bring supporting computers and hardware to run their demos, including the hardware shipped by the contest organizers. No extra equipment will be handed out at UIST.
+powering your demos at the contest: a Japanese-standard power strip will be provided for each team at their demo table. However, we recommend that you bring your own regional power strip with the appropriate US adapter to connect all your hardware through your own strip with a single adapter.
+WiFi availability: WiFi in the conference hall will likely be slow, however teams are welcome to use WiFi as a part of their demos. We recommend having a Plan B in case WiFi is too slow in the conference center.
+questions: if you have any questions about the contest rules, please contact the contest chairs.
+AWARDS
+Innovation comes in many forms, and the UIST community prides itself on both its hardware and its software. Thus, we've decided to award first and second prize in the categories below:
+Most Creative Interface (Judge's decision)
+Best Implementation  (Judge's decision)
+People's Choice (attendee votes during the demo session)
+We reserve the right to change the awards structure based on any factors, including but not limited to the number of teams participating.
+FAQ
+Hardware and Software
+Q: What is EMS?  A: EMS is Electrical Muscle Stimulation. It is a technique from rehabilitation medicine that allows to contract muscles by sending small (mA scale) electrical impulses via  electrodes attached to the skin.
+
+
+
+
 <div class="section about"><div class="blk-1">
 
 <h1 id="student-innovation-contest">Student Innovation Contest</h1>
@@ -34,7 +140,7 @@
 </ul>
 <h2 id="contest-rules-and-details">CONTEST RULES AND DETAILS</h2>
 <ul>
-<li>pre-selection criteria: 
+<li>pre-selection criteria:
 organizers will select a maximum of 30 teams, judging your demo descriptions and sketches by the following criteria: complexity of idea, time and resources required to build, appropriateness for high school animatronics users, creativity of idea, and usefulness of idea. </li>
 <li>team size: the minimum team size is 1, the maximum is 4</li>
 <li>team composition: an individual student cannot be a part of multiple teams</li>
@@ -62,9 +168,9 @@ organizers will select a maximum of 30 teams, judging your demo descriptions and
 <p>We reserve the right to change the awards structure based on any factors, including but not limited to the number of teams participating.</p>
 <h2 id="faq">FAQ</h2>
 <h3 id="registration-and-eligibility">Registration and Eligibility</h3>
-<p>Q: I am not a student, can I still participate? 
+<p>Q: I am not a student, can I still participate?
 A: Unfortunately, no. The contest is limited to students at all levels (High School, Bachelor&#39;s, Master&#39;s, and PhD).</p>
-<p>Q: I&#39;m in high school, can I participate in the contest? 
+<p>Q: I&#39;m in high school, can I participate in the contest?
 A: All students are welcome. However, you will have to be old enough (at least 18 years old) and able to travel to the UIST Conference in Waikiki, Hawaii.</p>
 <p>Q: I&#39;m an international student, can I participate in the contest?
 A: Yes. International students at all levels are encouraged to participate. You will be responsible for your own travel and arrangements.</p>
