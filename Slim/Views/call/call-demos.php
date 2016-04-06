cb836ad8c4e78f98e07aee93e8cbcac47654c1e4
Ryo Suzuki <ryosuzk@gmail.com>
2016-04-06 12:50:18 -0600

    Finalize demo pages

--- Slim/Views/call/call-demos.php
+++ Slim/Views/call/call-demos.php
@@ -1,7 +1,11 @@
+
 <div class="ui stackable grid">
+  <section id="call" class="twelve wide column centered">
+  <h2 class="ui horizontal divider header">Call for Demos</h2>
   <div class="ui stackable grid detail">
   <div class="fourteen wide column centered">
 
+  <!-- <h1>Call for Demos</h1> -->
   <h2>
     DEMONSTRATIONS (Deadline: July 7, 2016)
   </h2>
@@ -12,23 +16,23 @@
     <br><br>
     A demo submission should follow the official SIGCHI conference publication paper format, and should be no more than two pages in length. Note that demo submissions, unlike paper submissions, are not anonymous.
   </div>
-  <h3>Selection process</h3>
+  <h2>Selection process</h2>
   <div class="text">
     The selection process for UIST demos is curated, that is, selected by an esteemed committee, but not sent out to external reviewers. The curated demo content will be selected from demonstration submissions, accepted papers, as well as projects invited by the demo chairs. Authors will not receive formal feedback on their submission other than the selection decision.
   </div>
 
-  <h4>Note:</h4>
+  <h2>Note:</h2>
   <div class="text">
     The demo submission should include a (mock-up) photo of the demonstration showing the envisioned setup (demo hardware components, tables, chairs, lights and other equipment), as well as the demonstrators alongside conference attendees interacting with the demo. Please label and annotate all parts of the image(s) so that organizers get a good understanding of your setup. This will also allow the organizers to better consider your requirements when organizing the demonstration space at the conference. Submitting an accompanying video is optional, but highly encouraged. Any submitted video should be no longer than three minutes and at most 50MB in size.
   </div>
 
-  <h3>Demos are non-archival</h3>
+  <h2>Demos are non-archival</h2>
   <div class="text">
     UIST demos are non-archival and UIST allows resubmission of demos previously shown at other venues. Authors should state previous demo venues in the appropriate box in the upload form and point out differences to previous demos.
     <br><br>
-    See the <a href="video-guide">video guide</a> and the general submission information for more details about preparing your submission. Both the demo abstract and the optional digital video should be submitted electronically to Precision Conference.
+    See the <a href="https://uist.acm.org/uist2016/video-guide" target="_blank">video guide</a> and the general submission information for more details about preparing your submission. Both the demo abstract and the optional digital video should be submitted electronically to Precision Conference.
   </div>
-  <h3>Relevant dates:</h3>
+  <h2>Relevant dates:</h2>
   <div class="text">
     <ul>
       <li><b>July 9:</b> Submissions due by 17:00 PDT.</li>
