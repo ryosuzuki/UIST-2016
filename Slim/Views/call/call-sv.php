e20e268524f773979ca52bbbaa57abb5f3f52019
Takuma YOSHITANI <takuma.7@gmail.com>
2016-04-07 01:02:08 +0900

    markup call-sv.php

--- Slim/Views/call/call-sv.php
+++ Slim/Views/call/call-sv.php
@@ -11,63 +11,62 @@
   </h2>
 
   <div class="text">
-Application Instructions:
+  </div>
 
-To apply to be a student volunteer, please sign up at the UIST SV portal.
+  <h2>
+    Application Instructions:
+  </h2>
+  <div class="text">
+    To apply to be a student volunteer, please sign up at the <a href="http://chisv.org/uist2016">UIST SV portal</a>.
+  </div>
 
-Questions?
+  <h2>
+    Questions?
+  </h2>
+  <div class="text">
+    If you have any questions/problems, please contact the chairs at <a href="mailto:sv@uist.org">sv@uist.org</a>.
+  </div>
 
-If you have any questions/problems, please contact the chairs at sv@uist.org.
-
-Benefits and Duties:
-
-If you are a UIST Student Volunteer, you get:
-
- Free conference registration
-
- Free T-shirt
-
- A great SV party with free dinner
-
- A chance to attend the premier forum for innovations in human-computer interfaces
-
-Student volunteers will be required to help set up and perform needed work during the
-
-conference. Duties include: bag stuffing, registration desk, AV assistance, poster and demo
-
-setup, badge checking, break monitoring, etc. Volunteers will also need to be available during
-
-the conference if tasks come up. SVs usually get to attend most of the sessions, even when
-
-working. There will be approximately 14 scheduled hours per SV.
-
-Arriving and Leaving: SVs need to be available to help throughout the conference. You will
-
-need to arrive on or before Saturday afternoon (Oct. 15) for the SV orientation and should not
-
-leave the conference venue until sessions end on Wednesday (Oct. 19). Also, we strongly
-
-encourage you to put off leaving until as late as you possibly can, because it's traditional to have
-
-a small party for student volunteers on Wednesday evening. The time and place for this party
-
-will be announced when we know all the student volunteers' travel plans.
-
-Work Schedules: To give you an idea of what to expect, you can see the UIST 2006 Student
-
-Volunteer Schedule.
-
-Accommodation: We will not be able to provide free accommodation, but we will help you
-
-team up with other student volunteers for sharing rooms and we will provide a list of hotels with
-
-affordable room rates.
-
-
-
-
-
-    </div>
+  <h2>
+    Benefits and Duties:
+  </h2>
+  <div class="text">
+    If you are a UIST Student Volunteer, you get:
+    <ul>
+      <li>Free conference registration</li>
+      <li>Free T-shirt</li>
+      <li>A great SV party with free dinner</li>
+      <li>A chance to attend the premier forum for innovations in human-computer
+        interfaces</li>
+    </ul>
+      Student volunteers will be required to help set up and perform needed work
+      during the conference. Duties include: bag stuffing, registration desk, AV
+      assistance, poster and demo setup, badge checking, break monitoring, etc.
+      Volunteers will also need to be available during the conference if tasks
+      come up. SVs usually get to attend most of the sessions, even when working.
+      There will be approximately 14 scheduled hours per SV.
+  </div>
+  <h3>Arriving and Leaving:</h3>
+  <div class="text">
+    SVs need to be available to help throughout the conference. You will
+    need to arrive on or before Saturday afternoon (Oct. 15) for the SV
+    orientation and should not leave the conference venue until sessions end on
+    Wednesday (Oct. 19). Also, we strongly encourage you to put off leaving
+    until as late as you possibly can, because it's traditional to have a small
+    party for student volunteers on Wednesday evening. The time and place for
+    this party will be announced when we know all the student volunteers' travel
+    plans.
+  </div>
+  <h3>Work Schedules:</h3>
+  <div class="text">
+    To give you an idea of what to expect, you can see the <a href="http://www.acm.org/uist/uist2008/uist06schedule.html">UIST 2006 Student Volunteer Schedule</a>.
+  </div>
+  <h3>Accommodation:</h3>
+  <div class="text">
+    We will not be able to provide free accommodation, but we will help you
+    team up with other student volunteers for sharing rooms and we will provide
+    a list of hotels with affordable room rates.
+  </div>
   <h2>
     Relevant dates:
   </h2>
@@ -90,4 +89,3 @@ affordable room rates.
   </div>
 </section>
 </div>
-
