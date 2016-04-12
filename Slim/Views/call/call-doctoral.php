49d7e3c20f12d53ea8fc209909c8ba37fc3014a0
Takuma YOSHITANI <takuma.7@gmail.com>
2016-04-12 15:58:10 +0900

    add docoral symposium cfp

--- Slim/Views/call/call-doctoral.php
+++ Slim/Views/call/call-doctoral.php
@@ -1,24 +1,64 @@
-<div class="section about"><div class="blk-1">
-<h1 id="doctoral-symposium">Doctoral Symposium</h1>
-<h3 id="deadline-july-9-2015-17-00-pdt-">Deadline: July 9, 2015 17:00 PDT.</h3>
-<p>The UIST Doctoral Symposium is a forum in which Ph.D. students can meet and discuss their work with each other and a panel of experienced UIST researchers in an informal and interactive setting. We welcome applications from current Ph.D. students studying within the full range of disciplines and approaches that contribute to the UIST community. We will give preference to applicants beyond the proposal stage and well into their dissertation research.</p>
-<p>Each applicant should provide a short written paper (no more than four pages in the official SIGCHI conference publication format, as described in the authors&#39; guide). Note that doctoral symposium submission, unlike paper submission, is not anonymous. This paper should describe ongoing work and might summarize the student&#39;s full dissertation work, or highlight a particular part in depth.</p>
-<p>The Doctoral Symposium committee will select approximately eight students to participate. Each student will be expected to give a short presentation of their work, which will be followed by extensive discussion with the panel and the other student participants. The symposium will be held on November 8th. In addition, each student is encouraged to present a poster describing his or her work to the full conference; posters will be on display for two days during the conference.</p>
-<p>Participants will be selected based on their anticipated contributions to the breadth and depth of the intellectual discussions of the symposium.</p>
-<p>The symposium will start with an informal dinner on November 7th, continue all the following day, and conclude with poster presentations. Doctoral Symposium abstracts will be published together with poster and demonstration abstracts and made available digitally to conference attendees. Posters may also be published at the participant&#39;s discretion. In the past, a travel stipend and free registration to the UIST conference has been provided to each participant and we expect to be able to do the same this year.</p>
-<p>Doctoral Symposium papers and poster sketches should be submitted electronically to Precision Conference.</p>
-<h3 id="relevant-dates">Relevant dates</h3>
-<ul>
-<li><b>July 9:</b> Submissions due by 17:00 PDT.</li>
-<li><b>August 6:</b> Notification of acceptance decisions.</li>
-<li><b>August 23:</b> Camera ready versions due.</li>
-<li><b>November 8:</b> Doctoral Symposium date</li>
-</ul>
-<h3 id="doctoral-symposium-chair-doctoralsymposium-uist-org-mailto-doctoralsymposium-uist-org-">Doctoral Symposium Chair (<a href="mailto:doctoralsymposium@uist.org">doctoralsymposium@uist.org</a>)</h3>
-<ul>
-<li>Patrick Baudisch, Hasso Plattner Institute</li>
-</ul>
+<div class="ui stackable grid">
+  <section id="call" class="twelve wide column centered">
+  <div class="ui stackable grid detail">
+    <div class="fourteen wide column centered">
 
+    <h1>Call for Posters</h1>
+    <h3>Doctoral Symposium (Deadline: July 7, 2016 12:00 PDT)</h3>
+    <div class="text">
+      The UIST Doctoral Symposium is a forum in which Ph.D. students can meet and discuss their work with each other and a panel of experienced UIST researchers in an informal and interactive setting. We welcome applications from current Ph.D. students studying within the full range of disciplines and approaches that contribute to the UIST community. We will give preference to applicants beyond the proposal stage and well into their dissertation research.
+      <br>
+      <br>
+      Each applicant should provide a short written paper (no more than four pages in the official SIGCHI conference publication format(
 
+      <a href="http://www.sigchi.org/publications/chipubform/sigchi-papers-word-template/view" target="_blank">Word Template</a>
 
-</div></div>
+       or
+
+      <a href="http://www.sigchi.org/publications/chipubform/sigchi-papers-latex-template/view" target="_blank">Latex Template</a>
+
+      ), as described in
+
+      <a href="https://uist.acm.org/uist2016/author-guide" target="_blank">the author's guide</a>
+
+      ). Note that doctoral symposium submission, unlike paper submission, is not anonymous. This paper should describe ongoing work and might summarize the student's full dissertation work, or highlight a particular part in depth.
+
+      <br>
+      <br>
+
+      The Doctoral Symposium committee will select approximately eight students to participate. Each student will be expected to give a short presentation of their work, which will be followed by extensive discussion with the panel and the other student participants. The symposium will be held on <b>October 16th</b>. In addition, each student is encouraged to present a poster describing his or her work to the full conference; posters will be on display for two days during the conference.Participants will be selected based on their anticipated contributions to the breadth and depth of the intellectual discussions of the symposium.
+
+      <br>
+      <br>
+
+      The symposium will start with an informal dinner on October 15th, continue all the following day, and conclude with poster presentations. Doctoral Symposium abstracts will be published together with poster and demonstration abstracts and made available digitally to conference attendees. Posters may also be published at the participant's discretion. In the past, a travel stipend and free registration to the UIST conference has been provided to each participant and we expect to be able to do the same this year.
+      Doctoral Symposium papers and poster sketches should be submitted electronically to Precision Conference.
+    </div>
+    <h2>
+      Relevant dates:
+    </h2>
+    <div class="text">
+      <ul>
+        <li>
+          <b>July 7, 2016:</b> Submissions due by 12:00 PDT.
+        </li>
+        <li>
+          <b>July 30, 2016:</b> Notification of acceptance decisions.
+        </li>
+        <li>
+          <b>August 7, 2016:</b> Camera ready versions due.
+        </li>
+        <li>
+          <b>October 16, 2016:</b> Doctoral Sympposium date
+        </li>
+      </ul>
+    </div>
+
+    <h2>
+      Doctoral Symposium Chairs (<a href="mailto:doctoralsymposium@uist.org">doctoralsymposium@uist.org</a>)
+    </h2>
+    <div class="text">
+      <b>Patrick Baudisch</b> (Hasso Plattner Institute, Germany), <b>Hiromi Nakamura</b>  (The University of Tokyo, Japan)
+    </div>
+</section>
+</div>
