82fae5895d2d23e2ec41ca683872f71a767d0488
Ryo Suzuki <ryosuzk@gmail.com>
2016-04-19 20:17:39 -0600

    Add images in student innovatino contest

--- Slim/Views/call/call-sic.php
+++ Slim/Views/call/call-sic.php
@@ -9,8 +9,12 @@
   <div class="text">
     Welcome to the eighth annual UIST Student Innovation Contest (SIC) taking place in Tokyo!
     <br><br>
-    <strong>TL;DR:</strong> The next quantum leap in human-computer interfaces is making the feeling the virtual world. In this contest you will use a simple, yet powerful technology borrowed from rehabilitation medicine, to make this possible: Electrical Muscle Stimulation. Using EMS you can create strong physical sensations such as forces with wearable hardware. Join the UIST SIC and be one of the first to develop new exciting haptic sensations for applications such as virtual and augmented reality! Win fabulous prizes! Sign up at <a href="http://tinyurl.com/uistsic2016">http://tinyurl.com/uistsic2016</a> by August 1st of 2016.
-    <br><br>
+    <img class="ui medium right floated image" src="<?php echo BASE_PATH; ?>/lib/img/call/sic/impacto.jpg">
+    <strong>Summary</strong>
+    The next quantum leap in human-computer interfaces is making the feeling the virtual world. In this contest you will use a simple, yet powerful technology borrowed from rehabilitation medicine, to make this possible: Electrical Muscle Stimulation. Using EMS you can create strong physical sensations such as forces with wearable hardware. Join the UIST SIC and be one of the first to develop new exciting haptic sensations for applications such as virtual and augmented reality! Win fabulous prizes! Sign up at <a href="http://tinyurl.com/uistsic2016">http://tinyurl.com/uistsic2016</a> by August 1st of 2016.
+    <br>
+    <br>
+    <img class="ui small left floated image" src="<?php echo BASE_PATH; ?>/lib/img/call/sic/impacto-2.jpg">
     In the UIST Student Innovation Contest student teams create intriguing novel interfaces using exciting hardware kits that we provide, this year it is: Electrical Muscle Stimulation (EMS). It’s your opportunity to shine and impress the world with your innovations! Contestants will demo their creations during the demo reception at the conference in Tokyo Japan; and the winners will be announced at the UIST banquet. A jury of UIST celebrities will select two winners in the Most Creative Interface and Best Implementation categories. On top of that, conference attendees will get a chance to vote for their favorite teams in the People's Choice category.
   </div>
 
@@ -28,32 +32,32 @@
 
   <h2>CONTEST ANNOUNCEMENT</h2>
   <div class="text">
+    <img class="ui medium right floated image" src="<?php echo BASE_PATH; ?>/lib/img/call/sic/boards.jpg">
     For 2016, the UIST Student Innovation Contest takes on a very timely subject which is the next frontier in virtual and augmented reality: feeling the virtual (haptics)! You will help create a world in which virtual interfaces are no longer just wireframes without mass... you can use electrical muscle stimulation to create physical forces from the virtual world and... you can even make it mobile/wearable so that these interfaces are with us everywhere!
-    <br><br>
+    <br>
+    <br>
     Each team will receive a kit of parts including an arduino nano, the medically-safe muscle stimulator, electrodes and our usb/bluetooth control board. Using these parts, teams will be able to build virtual experiences, devices, interfaces that demonstrate how to make the virtual physical. We will also be providing you with simple examples on how to get started using <a href="https://www.arduino.cc/">Arduino</a>, <a href="https://unity3d.com">Unity3D</a> and <a href="https://processing.org">Processing</a>. All teams are encouraged to open source their efforts, and even build on each other’s work. To get you started easily, we will provide you, besides all the hardware, video tutorials and coaching sessions so you know how to setup everything and debug any questions you might have!
   </div>
   <h3>Demo applications and more ideas to build</h3>
+  <div class="ui images">
+    <img class="ui centered image" src="<?php echo BASE_PATH; ?>/lib/img/call/sic/muscle.jpg" width="35%">
+    <img class="ui centered image" src="<?php echo BASE_PATH; ?>/lib/img/call/sic/wearable.jpg" width="24%">
+    <img class="ui centered image" src="<?php echo BASE_PATH; ?>/lib/img/call/sic/prototype.jpg" width="35%">
+  </div>
   <div class="text">
-    The simplest demo you can build is one that renders a virtual event into a
-    physical event, for instance a mobile phone game in which you actually steer
-    an airplane against the wind (spoiler alert: we've done this one, check out
-    the <a href="https://www.youtube.com/watch?v=swmGaGT8lvs">video here</a>). Because using EMS makes interfaces more physical, when the
-    wind comes one of the player's wrists contracts (using the muscle stimulation)
-    and turns the airplane into an undesired direction, thus the player must turn
-    against the force-feedback and steer the airplane into safety! <strong>What other
-    futuristic mobile apps can you make that use EMS?</strong> A more compelling demo
-    that you can build takes place in Virtual Reality, what could you improve in
-    the experience of virtual reality if the user could feel forces? Check this
-    <a href="https://www.youtube.com/watch?v=k5e4mXQLq54">video example</a> for inspiration, here you see Virtual Reality sport simulators
-    such as boxing and baseball that actually deliver a force. Here we combined
-    the EMS bracelet with a solenoid... <strong>what other novel techniques and devices
-    can you combine EMS with?</strong> You can even combine it with everyday items and make
-    them smarter: what if while the user is manipulating an object you could actuate
-    and change the way they physically handle the object? Check this <a href="https://www.youtube.com/watch?v=Gz4dphzBb6I">video for
-    inspiration in which we made a spray can shake by itself</a>. <strong>Think outside the
-    box and create novel interfaces that take advantage of these physical
-    sensations for augmented or virtual reality</strong> or maybe you don't even need
-    screens! The more exciting your demo gets the higher are the chances for
+    The simplest demo you can build is one that renders a virtual event into a physical event, for instance a mobile phone game in which you actually steer an airplane against the wind (spoiler alert: we've done this one, check out the <a href="https://www.youtube.com/watch?v=swmGaGT8lvs">video here</a>). Because using EMS makes interfaces more physical, when the wind comes one of the player's wrists contracts (using the muscle stimulation) and turns the airplane into an undesired direction, thus the player must turn against the force-feedback and steer the airplane into safety!
+    <strong>
+      What other futuristic mobile apps can you make that use EMS?
+    </strong>
+    A more compelling demo that you can build takes place in Virtual Reality, what could you improve in the experience of virtual reality if the user could feel forces? Check this <a href="https://www.youtube.com/watch?v=k5e4mXQLq54">video example</a> for inspiration, here you see Virtual Reality sport simulators such as boxing and baseball that actually deliver a force. Here we combined the EMS bracelet with a solenoid...
+    <strong>
+      what other novel techniques and devices can you combine EMS with?
+    </strong>
+    You can even combine it with everyday items and make them smarter: what if while the user is manipulating an object you could actuate and change the way they physically handle the object? Check this <a href="https://www.youtube.com/watch?v=Gz4dphzBb6I" target="_balnk">video for inspiration in which we made a spray can shake by itself</a>. What about <b>teaching a new skill</b> using electrical muscle stimulation? Maybe playing a new instrument: get inspired with the amazing <a href="https://www.youtube.com/watch?v=9XBoZyfB8hY" target="_blank">Possessed Hand video</a>.
+    <strong>
+      Think outside the box and create novel interfaces that take advantage of these physical sensations for augmented or virtual reality
+    </strong>
+    or maybe you don't even need screens! The more exciting your demo gets the higher are the chances for
     the prize committee to be impressed.
   </div>
 
