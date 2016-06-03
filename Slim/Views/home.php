22777c0d837325820f287d4b83af5135e3db1e7f
Ryo Suzuki <ryosuzk@gmail.com>
2016-06-03 16:33:01 -0700

    Fix

--- Slim/Views/home.php
+++ Slim/Views/home.php
@@ -12,16 +12,18 @@
           <a href="http://precisionconference.com/~sigchi" target="_balnk">http://precisionconference.com/~sigchi</a>
           </li>
           <br>
-          <li><b>Upcoming Important Dates:</b><br>
+          <li><b>Upcoming Events:</b><br>
           <b>June 9-16, 2016</b>: The rebuttal period for <a href="http://uist.acm.org/uist2016/posters">Papers</a>
+          <br>
+          <b>June 26, 2016</b>: Authors will be notified the decision.
           <br>
           <b>July 7, 2016</b>: The deadline for <a href="http://uist.acm.org/uist2016/posters">Posters</a>
           <br>
           <b>July 7, 2016</b>: The deadline for <a href="http://uist.acm.org/uist2016/demos">Demos</a>
           <br>
-          <b>July 7, 2016</b>: The deadline <a href="http://uist.acm.org/uist2016/doctoral">Doctoral Symposium</a>
+          <b>July 7, 2016</b>: The deadline for <a href="http://uist.acm.org/uist2016/doctoral">Doctoral Symposium</a>
           <br>
-          <b>July 25, 2016</b>: The deadline <a href="http://uist.acm.org/uist2016/volunteers">Student Volunteers</a>
+          <b>July 25, 2016</b>: The deadline for <a href="http://uist.acm.org/uist2016/volunteers">Student Volunteers</a>
         </ul>
       </div>
     </div>
