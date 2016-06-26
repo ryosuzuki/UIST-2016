98b289bce845cd0fc65a56c2aae09af9c975000a
Ryo Suzuki <ryosuzk@gmail.com>
2016-06-25 20:45:42 -0700

    Copy registration section

---
+++ Slim/Views/registration.php
@@ -0,0 +1,75 @@
+
+<div class="ui stackable grid">
+  <section class="twelve wide column centered">
+    <h2 class="ui horizontal divider header">Registration</h2>
+    <div class="ui stackable grid detail">
+      <div class="fourteen wide column centered">
+        <h1>Registration Rates</h1>
+        <div class="text">
+          For early bird registration only (July 3-September 16) there are discount registration rates available (All rates in USD):
+        </div>
+
+        <table class="ui fixed single line table">
+          <thead class="text">
+            <tr>
+              <th>Early-bird (by September 16)</th>
+              <th>Regular (by September 30)</th>
+              <th>Late / On-site</th>
+            </tr>
+          </thead>
+          <tbody class="text">
+            <tr>
+              <td></td>
+            </tr>
+            <tr>
+              <td>September 30, 2016</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div class="text">
+          There is no discounts for registering for Both ACM UIST and ACM SUI. <br>
+          Use the UIST 2016 online registration system to register for the conference.
+          <a href="" class="ui green button">Registration</a>
+        </div>
+
+        <h1>Cancellation Policy</h1>
+        <div class="text">
+          Prior to <b>September 19, 2016</b>, cancellations will be refunded in full (minus credit card charges). Cancellations received from <b>September 20, 2016 through October 3, 2016</b> will be charged a 50% cancellation fee. No refunds will be given after <b>October 3, 2016</b>.
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+
+<div class="ui stackable grid">
+  <section class="twelve wide column centered">
+    <h2 class="ui horizontal divider header">Hotel Booking</h2>
+    <div class="ui stackable grid detail">
+      <div class="fourteen wide column centered">
+        <div class="text">
+          The conference venue is a university facility and we do not have any designated conference hotels. Please book a nearby hotel room by yourself. Early booking is strongly recommendaed becuase of constant shortage of hotel rooms in Tokyo. There are several hotels around the conference venue within walking distance. <a href="https://www.google.com/maps/d/edit?mid=zEQrhkQOwr5o.ks2qkUiXn81c&usp=sharing" target="_blank"><b><i class="fa fa-map-o fa-fw"></i>&nbsp;Open Google Maps</b></a>
+        </div>
+        <br>
+        <div class="text">
+          As an option, you can book a hotel through this web site (<a href="https://mice3.jtbgmt.com/uist2016/">https://mice3.jtbgmt.com/uist2016/</a>). The travel agency (JTB) secured a block of hotel rooms for us. However, the number of rooms are limited and we expect that this will be full soon. Afterwards, please book a hotel using any other service by yourself.
+          <br>
+          If you have any question about booking through the above web site, please directly contact JTB (<a href="mailto:uist2016@gmt.jtb.jp">uist2016@gmt.jtb.jp</a>)
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+
+<div class="ui stackable grid">
+  <section class="twelve wide column centered">
+    <h2 class="ui horizontal divider header">Visas</h2>
+    <div class="ui stackable grid detail">
+      <div class="fourteen wide column centered">
+        <div class="text">
+          If you need VISA to enter Japan, please request an invitation letter from <a href="http://secretari.jp/For_Visa_Application/"></a>http://secretari.jp/For_Visa_Application/  AFTER registration.
+Please follow instructions in the page.
+
+
