4b980446dd87924f36f1f6771f9f2ea31c6e1313
Ryo Suzuki <ryosuzk@gmail.com>
2016-08-12 10:14:29 -0700

    Add schedule

--- Slim/Views/schedule.php
+++ Slim/Views/schedule.php
@@ -3,56 +3,423 @@
 
 
 <div id="schedule" class="ui stackable grid">
-  <section class="sixteen wide column centered">
+  <section class="twelve wide column centered">
     <h2 class="ui horizontal divider header">Schedule</h2>
     <div class="ui stackable">
+      <table class="ui fixed single line celled table">
+        <thead class="text">
+          <tr>
+            <th colspan="4">
+              Sunday, October 16, 2016
+            </th>
+          </tr>
+        </thead>
+        <tbody class="text">
+          <tr>
+            <td>
+              16:00 - 20:00
+            </td>
+            <td colspan="2">
+              Registration
+            </td>
+            <td>
+              Hitotsubashi hall 1F
+            </td>
+          </tr>
+          <tr>
+            <td>
+              18:00 - 20:00
+            </td>
+            <td colspan="2">
+              Welcome Reception
+            </td>
+            <td>
+              Josui kaikan
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <table class="ui fixed celled table">
+        <thead class="text">
+          <tr>
+            <th colspan="4">
+              Monday, October 17, 2016
+            </th>
+          </tr>
+        </thead>
+        <tbody class="text">
+          <tr>
+            <td>
+              8:30 - 17:20
+            </td>
+            <td colspan="2">
+              Registration
+            </td>
+            <td>
+              Hitotsubashi hall 1F
+            </td>
+          </tr>
+          <tr>
+            <td>
+              8:45 - 9:30
+            </td>
+            <td colspan="2">
+              Refreshment (Coffee & Snacks)
+            </td>
+            <td>
+              Hitotsubashi hall 2F
+            </td>
+          </tr>
+          <tr>
+            <td>
+              9:30 - 10:00
+            </td>
+            <td colspan="2">
+              Opening Remarks
+            </td>
+            <td>
+              Hitotsubashi hall 2F
+            </td>
+          </tr>
+          <tr>
+            <td>
+              10:00 - 11:00
+            </td>
+            <td colspan="2">
+              Keynote: Takeo Kanade
+            </td>
+            <td>
+              Hitotsubashi hall 2F Auditorium
+            </td>
+          </tr>
+          <tr>
+            <td class="active">
+              11:00 - 11:20
+            </td>
+            <td colspan="3" class="active">
+              Coffee Break (20min)
+            </td>
+          </tr>
+          <tr>
+            <td>
+              11:20 - 12:40
+            </td>
+            <td colspan="2">
+              Session 1A: Fabrication
+              <br>
+              Session 1B: Touch and Beyond
+            </td>
+            <td>
+              Hitotsubashi hall 2F Auditorium / Ballroom
+            </td>
+          </tr>
+          <tr>
+            <td class="active">
+              12:40 - 14:00
+            </td>
+            <td colspan="3" class="active">
+              Lunch (Lunchbox provided)
+              <br>
+              Women’s luncheon (1F meeting room)
+            </td>
+          </tr>
+          <tr>
+            <td>
+              14:00 - 15:20
+            </td>
+            <td colspan="2">
+              Session 2A: TUI
+              <br>
+              Session 2B: Interaction Techniques
+            </td>
+            <td>
+              Hitotsubashi hall
+                 2F Auditorium
+                / Ballroom
+            </td>
+          </tr>
+          <tr>
+            <td class="active">
+              15:20 - 16:00
+            </td>
+            <td colspan="3" class="active">
+              Coffee Break (40min)
+            </td>
+          </tr>
+          <tr>
+            <td>
+              16:00 - 17:20
+            </td>
+            <td colspan="2">
+              Session 3A: Touch It, Feel It
+              <br>
+              Session 3B: Dev Tools
+            </td>
+            <td>
+              Hitotsubashi hall 2F Auditorium / Ballroom
+            </td>
+          <tr>
+            <td>
+              18:00 - 21:00
+            </td>
+            <td colspan="2">
+              Demo Reception
+            </td>
+            <td>
+              Josui Kaikan
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
 
-<table class="ui fixed single line table">
-  <thead class="text">
-    <tr>
-      <th>Time</th>
-      <th>Track</th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody class="text">
-    <tr>
-      <td><a href="http://uist.acm.org/uist2016/papers">Papers & Notes</a></td>
-      <td><b style="color:#ccc;">April 13 (Wed), 2016</b></td>
-    </tr>
-    <tr>
-      <td><a href="http://uist.acm.org/uist2016/posters">Posters</a></td>
-      <td><b style="color:#ccc;">July 7 (Thu), 2016</b></td>
-    </tr>
-    <tr>
-      <td><a href="http://uist.acm.org/uist2016/demos">Demos</a></td>
-      <td><b style="color:#ccc;">July 7 (Thu), 2016</b></td>
-    </tr>
-    <tr>
-      <td><a href="http://uist.acm.org/uist2016/doctoral">Doctoral Symposium</a></td>
-      <td><b style="color:#ccc;">July 7 (Thu), 2016</b></td>
-    </tr>
-    <tr>
-      <td><a href="http://uist.acm.org/uist2016/volunteers">Student Volunteers</a></td>
-      <td><b style="color:#ccc;">July 25 (Mon), 2016</b></td>
-    </tr>
-    <tr>
-      <td><a href="http://uist.acm.org/uist2016/contest">Student Innovation Contest</a></td>
-      <td><b>August 15 (Mon), 2016</b></td>
-    </tr>
-  </tbody>
-</table>
+      <table class="ui fixed celled table">
+        <thead class="text">
+          <tr>
+            <th colspan="4">
+              Tuesday, October 18, 2016
+            </th>
+          </tr>
+        </thead>
+        <tbody class="text">
+          <tr>
+            <td>
+              8:30 - 19:30
+            </td>
+            <td colspan="2">
+              Registration
+            </td>
+            <td>
+              Hitotsubashi hall 1F
+            </td>
+          </tr>
+          <tr>
+            <td>
+              8:45 - 9:30
+            </td>
+            <td colspan="2">
+              Refreshment (Coffee & Snacks)
+            </td>
+            <td>
+              Hitotsubashi hall 2F
+            </td>
+          </tr>
+          <tr>
+            <td>
+              9:30 - 10:50
+            </td>
+            <td colspan="2">
+              Session 4A: Touch
+              <br>
+              Session 4B: Sensing
+            </td>
+            <td>
+              Hitotsubashi hall 2F Auditorium / Ballroom
+            </td>
+          </tr>
+          <tr>
+            <td class="active">
+              10:50 - 11:20
+            </td>
+            <td colspan="3" class="active">
+              Coffee Break (40min)
+            </td>
+          </tr>
+          <tr>
+            <td>
+              11:20 - 12:40
+            </td>
+            <td colspan="2">
+              Session 5A: Viz
+              <br>
+              Session 5B: Physical Displays
+            </td>
+            <td>
+              Hitotsubashi hall 2F Auditorium / Ballroom
+            </td>
+          </tr>
+          <tr>
+            <td class="active">
+              12:40 - 14:00
+            </td>
+            <td colspan="3" class="active" class="active">
+              Lunch (Lunchbox provided)
+            </td>
+          </tr>
+          <tr>
+            <td>
+              14:00 - 15:00
+            </td>
+            <td colspan="2">
+              Session 6A: Information & Obfusc**ion
+              <br>
+              Session 6B: Video & Audio
+            </td>
+            <td>
+              Hitotsubashi hall 2F Auditorium / Ballroom
+            </td>
+          </tr>
+          <tr>
+            <td class="active">
+              15:00 - 15:40
+            </td>
+            <td colspan="3" class="active">
+              Coffee Break (40min)
+            </td>
+          </tr>
+          <tr>
+            <td>
+              15:40 - 17:00
+            </td>
+            <td colspan="2">
+              Session 7A: Fab with New Materials
+              <br>
+              Session 7B: Text Entry
+            </td>
+            <td>
+              Hitotsubashi hall 2F Auditorium / Ballroom
+            </td>
+          <tr>
+            <td>
+              17:00 - 19:30
+            </td>
+            <td colspan="2">
+              Poster & Student Innovation Competition (Coffee & Snacks)
+            </td>
+            <td>
+              Hitotsuabshi hall 2F
+            </td>
+          </tr>
+          <tr>
+            <td>
+              22:00 - 22:30
+            </td>
+            <td colspan="2">
+              Banquet
+            </td>
+            <td>
+              Hotel Grand Palace
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
+      <table class="ui fixed celled table">
+        <thead class="text">
+          <tr>
+            <th colspan="4">
+              Tuesday, October 19, 2016
+            </th>
+          </tr>
+        </thead>
+        <tbody class="text">
+          <tr>
+            <td>
+              8:30 - 12:40
+            </td>
+            <td colspan="2">
+              Registration
+            </td>
+            <td>
+              Hitotsubashi hall 1F
+            </td>
+          </tr>
+          <tr>
+            <td>
+              8:45 - 9:30
+            </td>
+            <td colspan="2">
+              Refreshment (Coffee & Snacks)
+            </td>
+            <td>
+              Hitotsubashi hall 2F
+            </td>
+          </tr>
+          <tr>
+            <td>
+              9:30 - 10:50
+            </td>
+            <td colspan="2">
+              Session 8A: Crowds
+              <br>
+              Session 8B: Electronics Printing & Prototyping
+            </td>
+            <td>
+              Hitotsubashi hall 2F Auditorium / Ballroom
+            </td>
+          </tr>
+          <tr>
+            <td class="active">
+              10:50 - 11:20
+            </td>
+            <td colspan="3" class="active">
+              Coffee Break (40min)
+            </td>
+          </tr>
+          <tr>
+            <td>11:20 - 12:40</td>
+            <td colspan="2">
+              Session 9A: ~Reality
+              <br>
+              Session 9B: Creativity
+            </td>
+            <td>Hitotsubashi hall 2F Auditorium / Ballroom</td>
+          </tr>
+          <tr>
+            <td class="active">
+              12:40 - 14:30
+            </td>
+            <td colspan="3" class="active">
+              Lunch (Lunchbox provided)
+              <br>
+              13:00 - 14:00 Town hall meeting @ Ballroom
+            </td>
+          </tr>
+          <tr>
+            <td>
+              14:30 - 15:50
+            </td>
+            <td colspan="2">
+              Session 10A: Innovative Interaction
+              <br>
+              Session 10B: Gesture
+            </td>
+            <td>Hitotsubashi hall 2F Auditorium / Ballroom</td>
+          </tr>
+          <tr>
+            <td class="active">
+              15:50 - 16:30
+            </td>
+            <td colspan="3" class="active">
+              Coffee Break (40min)
+            </td>
+          </tr>
+          <tr>
+            <td>
+              16:30 - 17:30
+            </td>
+            <td colspan="2">
+              Closing Keynote: Naoto Fukasawa
+            </td>
+            <td>
+              Hitotsubashi hall 2F Auditorium
+            </td>
+          </tr>
+          <tr>
+            <td>
+              17:30 - 18:00
+            </td>
+            <td colspan="2">
+              Closing ceremony
+            </td>
+            <td>
+              Hitotsuabshi hall 2F Auditorium
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
-
-      <div class="ui left dividing rail">
-        FWJFOWEJOF
-      </div>
-
-      <div class="fourteen wide column">
-        FJFWEOJFEOJ
-      </div>
     </div>
   </section>
 </div>
